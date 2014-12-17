3836b45e914b543027c69329f225772b1a114a1c
Nicholas Ogaye <owallaa@yahoo.com>
2014-12-17 01:58:25 -0500

    Added db credentials
    Added db credentials

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'moods',
+			'username'  => 'root',
+			'password'  => 'nyM3WdGLMc',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
